7ad3431787ab190fdbff252a6088a1f17aebc99f
Vencejo Software <vencejosoftware@gmail.com>
2017-12-06 16:08:06 -0300

    Added generics.collections to dependencies

--- batch/get_dependencies.bat
+++ batch/get_dependencies.bat
@@ -5,3 +5,8 @@ if not exist %delphiooLib%\ooBatch\ (
   git clone https://github.com/VencejoSoftware/ooBatch.git %delphiooLib%\ooBatch\
   call %delphiooLib%\ooBatch\code\get_dependencies.bat
 )
+
+if not exist %path_3rd_party%\generics.collections\ (
+  @echo "Clonning generics.collections..."
+  git clone https://github.com/VencejoSoftware/generics.collections.git %path_3rd_party%\generics.collections\
+)
