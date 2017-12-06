efe3f7bc6b351b2dc80f7a84396ca954d2d44548
Vencejo Software <vencejosoftware@gmail.com>
2017-12-06 16:51:37 -0300

    Fix script

--- batch/get_dependencies.bat
+++ batch/get_dependencies.bat
@@ -6,7 +6,7 @@ if not exist %delphiooLib%\ooBatch\ (
   call %delphiooLib%\ooBatch\code\get_dependencies.bat
 )
 
-if not exist %path_3rd_party%\generics.collections\ (
+if not exist %delphi3rdParty%\generics.collections\ (
   @echo "Clonning generics.collections..."
-  git clone https://github.com/VencejoSoftware/generics.collections.git %path_3rd_party%\generics.collections\
+  git clone https://github.com/VencejoSoftware/generics.collections.git %delphi3rdParty%\generics.collections\
 )
